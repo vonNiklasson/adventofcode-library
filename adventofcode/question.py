53c3cc30d4879e0dde56c63a2064d27b86f40deb
Johan Niklasson <johan.niklasson@klarna.com>
2020-12-04 11:51:48 +0100

    Added year and day assertion

--- adventofcode/question.py
+++ adventofcode/question.py
@@ -1,10 +1,19 @@
 import requests
+import warnings
 
 from . import utils
 
 
 class Question:
     def __init__(self, base_url: str, session_id: str, working_dir: str, year: int, day: int, part_two: bool = False):
+        # Raise a warning if the year is not in a tested year.
+        if year != 2020:
+            warnings.warn('adventofcode-library has only been tested with 2020 years calendar. Use at your own risk.')
+
+        # Make sure the day is within bounds.
+        if not 1 <= day <= 25:
+            raise IndexError('Day must be between 1 and 25 (inclusive).')
+
         self.base_url = base_url
         self.session_id = session_id
         self.working_dir = working_dir
